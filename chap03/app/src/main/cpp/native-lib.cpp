913021723c3dda2f8a3d5e1a75a810f6fe3482db
j2B237 <yossep237@gmail.com>
2025-12-26 20:06:28 +0000

    add more opengl ops

--- chap03/app/src/main/cpp/native-lib.cpp
+++ chap03/app/src/main/cpp/native-lib.cpp
@@ -86,21 +86,109 @@ GLuint loadShader(GLenum shaderType, const char *shaderSource){
 	return shader;
 }
 
+/*
+	createProgram().A program can be thought of as something that holds your shaders. 
+	It requires a vertex shader and a fragment shader.
+
+*/
+
+GLuint createProgram(const char *vertexSource, const char *fragmentSource){
+
+	GLuint vertex = loadShader(GL_VERTEX_SHADER, vertexSource);
+	if(!vertex)
+		return 0;
+
+	GLuint fragment = loadShader(GL_FRAGMENT_SHADER, fragmentSource);
+	if(!fragment)
+		return 0;
+
+	GLuint program = glCreateProgram();
+
+	if(program){
+
+		glAttachShader(program, vertex);
+		glAttachShader(program, fragment);
+		glLinkProgram(program);
+
+		GLint linked = GL_FALSE;
+		glGetProgramiv(program, GL_LINK_STATUS, &linked);
+
+		if(linked != GL_TRUE){
+			
+			GLint bufLen = 0;
+			glGetProgramiv(program, GL_INFO_LOG_LENGTH, &bufLen);
+
+			if(bufLen){
+				char *buf = (char*)malloc(bufLen * sizeof(char));
+
+				if(buf){
+					glGetProgramInfoLog(program, bufLen, NULL, buf);
+					LOGE("Could not link program:\n%s\n", buf);
+					free(buf);
+				}
+			}
+			glDeleteProgram(program);
+			program = 0;
+		}
+	}
+
+	return program;
+}
+
+/*
+	We now define two global variables. The first is the program. 
+	This will hold the value of the program that will be created 
+	by calling the above functions. 
+	The second will hold the location of where the GPU will be expecting 
+	the vertex data that is required for our shader.
+*/
+
+GLuint simpleTriangleProgram;
+GLuint vPosition;
+
+bool setupGraphics(int w, int h) {
+	simpleTriangleProgram = createProgram(glVertexShader, glFragmentShader);
+
+	if(!simpleTriangleProgram){
+		LOGE("Could not create a program");
+		return false;
+	}
+
+	vPosition = glGetAttribLocation(simpleTriangleProgram,"vPosition");
+	glViewport(0, 0, w, h);
+
+	return true;
+}
+
+const GLfloat triangleVertices[] = {
+   0.0f, 0.5f,
+  -0.5f, -0.5f,
+   0.5f, -0.5f
+};
+
+void renderFrame() {
+
+	glClearColor(0.3, 0.3, 0.3, 1.0);
+	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(simpleTriangleProgram);
+	glVertexAttribPointer(vPosition, 2, GL_FLOAT, GL_FALSE, 0, triangleVertices);
+	glEnableVertexAttribArray(vPosition);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
 
 JNIEXPORT void JNICALL 
 Java_com_example_simpleTriangle_NativeLibrary_init
-  (JNIEnv * env, jclass clazz, jint width, jint height)
-{
+  (JNIEnv * env, jclass clazz, jint width, jint height) {
 	/* sleeping to avoid thrashing the android log */
-	sleep(5),
-	LOGI("New Frame ready to be Drawn!!!!");
+	setupGraphics(width, height);
 }
 
 JNIEXPORT void JNICALL 
 Java_com_example_simpleTriangle_NativeLibrary_step
-  (JNIEnv *env, jclass clazz)
-{
-
+  (JNIEnv *env, jclass clazz){
+  	renderFrame();
 }
 #ifdef __cplusplus
 }
