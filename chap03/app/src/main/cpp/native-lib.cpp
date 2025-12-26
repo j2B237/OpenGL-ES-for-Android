f2a282d551685aa849c1bb3c0a9ef21c829a4036
j2B237 <yossep237@gmail.com>
2025-12-26 13:32:13 +0000

    chapter 03

---
+++ chap03/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,30 @@
+#include "com_example_simpleTriangle_NativeLibrary.h"
+#include <unistd.h>
+#include <android/log.h>
+
+#define LOG_TAG "libNative"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+JNIEXPORT void JNICALL 
+Java_com_example_simpleTriangle_NativeLibrary_init
+  (JNIEnv * env, jclass clazz, jint width, jint height)
+{
+	/* sleeping to avoid thrashing the android log */
+	sleep(5),
+	LOGI("New Frame ready to be Drawn!!!!");
+}
+
+JNIEXPORT void JNICALL 
+Java_com_example_simpleTriangle_NativeLibrary_step
+  (JNIEnv *env, jclass clazz)
+{
+
+}
+#ifdef __cplusplus
+}
+#endif
